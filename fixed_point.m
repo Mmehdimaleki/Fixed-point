2876595eb2032cb19106a4b682189b551046ba83
Mahdi Maleki <malekimed81@gmail.com>
2023-06-13 16:29:40 +0330

    added comments

--- fixed_point.m
+++ fixed_point.m
@@ -1,13 +1,17 @@
+%first we get the input
 g=input('g(x)=');
+%we get the first guess 
 x0=input('first guss=');
+%we get the number of iterations
 n=input('n=');
+%we input the error
 error=input('error=');
-while n>0
-    x1=g(x0);
-    x1;
-    if abs(x1-x0)<error
-        break
+       while n>0
+           x1=g(x0);
+           x1;
+       if abs(x1-x0)<error 
+       break
     end
 x0=x1;
 n=n-1;
-end
+    end
